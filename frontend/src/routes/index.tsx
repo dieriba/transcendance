ce3980877f17aee9e6442541f515c3346a87b14e
dieriba <dieriba.pro@gmail.com>
2023-10-22 05:25:10 +0200

    added loadable and settings route

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -6,7 +6,6 @@ import { Navigate, useRoutes } from "react-router-dom";
 import { DEFAULT_PATH } from "../config";
 import DashboardLayout from "../components/layouts/Dashboard";
 import Loadable from "./Loadable";
-import Chat from "../pages/Chats/Chat";
 
 export default function Router() {
   return useRoutes([
@@ -24,6 +23,7 @@ export default function Router() {
       children: [
         { element: <Navigate to={DEFAULT_PATH} replace />, index: true },
         { path: "chats", element: <Chat /> },
+        { path: "settings", element: <Settings /> },
         { path: "404", element: <Page404 /> },
         { path: "*", element: <Navigate to="/404" replace /> },
       ],
@@ -37,3 +37,5 @@ const Page404 = Loadable(() => import("../pages/error-page/Error-404"));
 
 const LoginPage = Loadable(() => import("../pages/auth/Login"));
 const RegisterPage = Loadable(() => import("../pages/auth/Register"));
+const Chat = Loadable(() => import("../pages/Chats/Chat"));
+const Settings = Loadable(() => import("../pages/Settings/Settings"));
